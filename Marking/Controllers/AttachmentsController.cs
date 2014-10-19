79e944cc54ce5fa3c96d84d084ebba861e0283e1
Al Capwn <gadonj18@live.com>
2014-10-18 20:56:01 -0600

    Got downloads working I think

---
+++ Marking/Controllers/AttachmentsController.cs
@@ -0,0 +1,42 @@
+﻿using Marking.DAL;
+using Marking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mime;
+
+namespace Marking.Controllers
+{
+    public class AttachmentsController : Controller
+    {
+        private MarkingContext db = new MarkingContext();
+
+        public ActionResult Download(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attachment attachment = db.Attachments.Find(id);
+            ContentDisposition cd = new ContentDisposition
+            { 
+                FileName = attachment.Filename,
+                Inline = false
+            };
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+            return new FilePathResult("~/Content/uploads/" + attachment.FilenameInternal, MediaTypeNames.Application.Octet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
